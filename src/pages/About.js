59e858b8c686d7129fce6052937839c55d360eba
GaryVH <gary.vanhecke@gmail.com>
2023-06-30 09:45:59 +0200

    Step2 - Add React Router Pages home, about and 404

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores maxime
+        perferendis suscipit nam. Quia deleniti dolor earum quaerat repellendus
+        explicabo officia voluptatum iste minima, voluptates beatae asperiores
+        iusto quibusdam distinctio illum obcaecati aperiam suscipit quam!
+        Provident minus corrupti eum repudiandae praesentium alias. Delectus
+        excepturi iste repellendus corrupti praesentium quos, voluptas commodi
+        perspiciatis adipisci numquam. Vitae architecto fugiat voluptates earum,
+        hic quidem dolore. Adipisci velit dolorem beatae alias labore.
+        Architecto in, vel, voluptas ipsa accusamus quibusdam accusantium unde
+        porro quo minima perspiciatis cum! Debitis quo non iusto dolore aut
+        veniam facilis dicta, deleniti aperiam mollitia. Delectus aspernatur
+        iste nihil laborum ipsa!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor rerum,
+        atque pariatur nulla fugit harum laboriosam error adipisci dicta veniam
+        et eius dolorum deserunt expedita perspiciatis ipsa ipsum delectus magni
+        tempora quis id? Qui architecto repudiandae ex eius tempore pariatur,
+        sint beatae eos, recusandae, ipsa quis illo. Consequuntur, nulla ipsum.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/PageNotFind.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageNotFind = () => {
+  return <div>ERREUR 404</div>;
+};
+
+export default PageNotFind;
