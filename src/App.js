bcdb8da64d89870eb901bdcaf6bc5518f9e79cd1
GaryVH <gary.vanhecke@gmail.com>
2023-07-25 21:54:31 +0200

    Add max width : 1440 px and margin auto

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Page404 from "./pages/Page404";
-import "./resets.css";
+import "./settings.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Lodging from "./pages/Lodging";
