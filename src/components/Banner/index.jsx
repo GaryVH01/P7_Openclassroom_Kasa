1fd54b7050e061dce16fcec1d926fe4db0b30735
GaryVH <gary.vanhecke@gmail.com>
2023-07-26 12:39:38 +0200

    h1 banner display flex to display in 2 lines in mobile

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./style.css";
-import BannerTitle from "components/BannerTitle";
 
 const Banner = (props) => {
   return (
     <div className="banner">
-      {/* <div className="divTitle"> */}
-        {/* <h1>{props.title}</h1>  */}
+      <div className="divTitle">
+        <h1><span>{props.titleLine1}</span><span>{props.titleLine2}</span></h1> 
         {/* On passe des props qui seront appelées dans le composant de la page en question */}
-      {/* </div> */}
-      <BannerTitle line1="Chez vous," line2="partout et ailleurs"/>
+      </div>
       <img
         className="imgBanner"
         src={props.src}
