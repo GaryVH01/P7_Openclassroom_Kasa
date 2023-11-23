ca8683f46634025735d92a8415431274081b2bfe
GaryVH <gary.vanhecke@gmail.com>
2023-11-23 15:45:51 +0100

    Improve formuser

--- src/components/Log/LoginForm.jsx
+++ src/components/Log/LoginForm.jsx
@@ -2,20 +2,41 @@ import React from "react";
 import "./LoginForm.css";
 import { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [action, setAction] = useState("S'INSCRIRE");
 
   return (
     <div className="login">
-      <h1 className="login_title">Connectez-vous</h1>
+      <h1 className="login_title">{action}</h1>
+      {action === "SE CONNECTER" ? (
+        <div></div>
+      ) : (
+        <div className="login__item">
+          {/* <label htmlFor="Login"></label> */}
+          <FaRegUser className="login__item__icon" />
+          <input type="text" placeholder="Nom d'utilisateur" />
+        </div>
+      )}
+
       <div className="login__item">
-        <label htmlFor="Login">Login</label>
-        <input type="text" />
+        {/* <label htmlFor="mail"></label> */}
+        <CiMail className="login__item__icon" />
+        <input type="text" placeholder="Adresse mail" />
       </div>
       <div className="login__item">
-        <label htmlFor="password">Password</label>
-        <input className="password" type={showPassword ? "text" : "password"} />
+        {/* <label htmlFor="password"></label> */}
+        <RiLockPasswordLine className="login__item__icon" />
+        <input
+          placeholder="Mot de passe"
+          className="password"
+          type={showPassword ? "text" : "password"}
+        />
         <div className="log-icons">
           {showPassword ? (
             <FaRegEyeSlash
@@ -30,11 +51,35 @@ const LoginForm = () => {
           )}
         </div>
       </div>
+      {action === "S'INSCRIRE" ? (
+        <div></div>
+      ) : (
+        <p className="lostpass">
+          Si vous avez égaré votre mot de passe,{" "}
+          <NavLink to="#">cliquez-ici</NavLink>
+        </p>
+      )}
 
-      <p>
-        Si vous n'êtes pas encore membre, <a href="">créez votre compte</a>
-      </p>
-      <button className="btn-connect">Se connecter</button>
+      <div className="submit">
+        <button
+          className={action === "S'INSCRIRE" ? "btn-signup red" : "btn-signup"}
+          onClick={() => {
+            setAction("S'INSCRIRE");
+          }}
+        >
+          S'INSCRIRE
+        </button>
+        <button
+          className={
+            action === "SE CONNECTER" ? "btn-connect red" : "btn-connect"
+          }
+          onClick={() => {
+            setAction("SE CONNECTER");
+          }}
+        >
+          Se connecter
+        </button>
+      </div>
     </div>
   );
 };
